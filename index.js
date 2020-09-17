208a72c69ea6ff378b8c57b6ca5e86dc228acf9c
Swapnil Karki <swapnilk21030@gmail.com>
2020-09-17 14:38:57 +0545

    url shorten / redirect

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const yup = require("yup");
+const monk = require("monk");
+const { nanoid } = require("nanoid");
+
+require("dotenv").config();
+
+const db = monk(process.env.MONGO_URI);
+const urls = db.get("urls");
+urls.createIndex({ slug: 1 }, { unique: true });
 
 const app = express();
 
@@ -11,18 +20,69 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("./public"));
 
-// app.get("/:id", (req, res) => {
-//   //todo: redirect to url
-// });
+app.get("/:id", async (req, res) => {
+  const { id: slug } = req.params;
+  try {
+    const url = await urls.findOne({ slug });
+    if (url) {
+      res.redirect(url.url);
+    }
+    res.redirect(`/?error=${slug} not found`);
+  } catch (error) {
+    res.redirect("/?error=Link not found");
+  }
+});
 
 // app.get("/url/:id", (req, res) => {
 //   //todo: get a short url by id
 // });
 
-// app.post("/url", (req, res) => {
-//   //todo: create a short url
-// });
+const schema = yup.object().shape({
+  slug: yup
+    .string()
+    .trim()
+    .matches(/[\w\-]/i),
+  url: yup.string().trim().url().required(),
+});
 
+app.post("/url", async (req, res, next) => {
+  let { slug, url } = req.body;
+  try {
+    await schema.validate({
+      slug,
+      url,
+    });
+    if (!slug) {
+      slug = nanoid(5);
+    } else {
+      const existing = await urls.findOne({ slug });
+      if (existing) {
+        throw new Error("Slug in Use");
+      }
+    }
+    slug = slug.toLowerCase();
+    const newUrl = {
+      url,
+      slug,
+    };
+    const created = await urls.insert(newUrl);
+    res.json(created);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === "production" ? "" : error.stack,
+  });
+});
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
